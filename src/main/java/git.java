acb309b5181a33ccc489c3198f1fccce19f8a679
mooli <mooli@qq.com>
2022-09-30 10:46:19 +0800

    first commit

---
+++ src/main/java/git.java
@@ -0,0 +1,10 @@
+/**
+ * @author idiotmooli
+ * @create 2022-09-30 10:42
+ * @introduce
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hellogit");
+    }
+}
