fa9673cc047ac4504dea85ead8ea4a9fa29983b3
mooli <mooli@qq.com>
2022-09-30 10:50:30 +0800

    third commit

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -7,5 +7,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hellogit");
         System.out.println("hellogit2");
+        System.out.println("hellogit3");
     }
 }
